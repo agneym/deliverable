5557393932aba344c6d3c1edadb054c6cbf1463a
Agney <agney.menon@bigbinary.com>
2020-03-18 01:27:08 +0530

    layout styling

--- components/Layout.js
+++ components/Layout.js
@@ -2,18 +2,38 @@ import React from "react";
 import { Layout as AntdLayout } from "antd";
 import styled from "styled-components";
 
-const { Header, Content, Footer: AntdFooter } = AntdLayout;
+const {
+  Header: AntdHeader,
+  Content: AntdContent,
+  Footer: AntdFooter,
+} = AntdLayout;
+
+const Header = styled(AntdHeader)`
+  position: fixed;
+  z-index: 1;
+  width: 100%;
+  background-color: ${props => props.theme.colors.white};
+  box-shadow: ${props => props.theme.boxShadows.default};
+`;
 
 const Footer = styled(AntdFooter)`
   background-color: ${props => props.theme.colors.gray[700]};
   color: ${props => props.theme.colors.gray[200]};
 `;
 
+const Content = styled(AntdContent)`
+  padding: 0 5rem;
+  margin: 64px auto 0;
+  min-height: calc(100vh - 64 * 2px);
+  max-width: 60%;
+  width: 100%;
+`;
+
 function Layout({ children }) {
   return (
     <AntdLayout>
-      <Header style={{ position: "fixed", zIndex: 1, width: "100%" }}></Header>
-      <Content style={{ padding: "0 50px", marginTop: 64 }}>{children}</Content>
+      <Header></Header>
+      <Content>{children}</Content>
       <Footer style={{ textAlign: "center" }}>Open Source on Github.</Footer>
     </AntdLayout>
   );
