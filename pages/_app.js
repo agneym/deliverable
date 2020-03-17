00d2519c65102944450b2c8c772e6932dbaeda39
Agney <agney.menon@bigbinary.com>
2020-03-18 00:40:43 +0530

    setup firebase

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,22 @@
-import App from 'next/app'
-import React from 'react'
-import { ThemeProvider } from 'styled-components';
+import App from "next/app";
+import React from "react";
+import { ThemeProvider } from "styled-components";
 
 import theme from "../lib/styles/theme";
 import Global from "../lib/styles/Global";
+import setupFirebase from "../lib/firebase/setup";
 
 export default class CustomApp extends App {
+  componentDidMount() {
+    setupFirebase();
+  }
   render() {
-    const { Component, pageProps } = this.props
+    const { Component, pageProps } = this.props;
     return (
       <ThemeProvider theme={theme}>
         <Component {...pageProps} />
         <Global />
       </ThemeProvider>
-    )
+    );
   }
 }
